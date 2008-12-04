384a5d35c93973bd7bc7a8db907b89223723f123
Jose' Valim <jose.valim@gmail.com>
2008-12-03 22:55:48 -0200

    Not using class_eval anymore

--- lib/localized_templates/actioncontroller/base.rb
+++ lib/localized_templates/actioncontroller/base.rb
@@ -1,8 +1,12 @@
-ActionController::Base.class_eval do
-  private
-    def template_exists?(template_name = default_template_name)
-      @template.send(:_pick_template, template_name, I18n.locale) ? true : false
-    rescue ActionView::MissingTemplate
-      false
-    end
+module ActionController
+  class Base
+
+    private
+      def template_exists?(template_name = default_template_name)
+        @template.send(:_pick_template, template_name, I18n.locale) ? true : false
+      rescue ActionView::MissingTemplate
+        false
+      end
+
+  end
 end
--- lib/localized_templates/actioncontroller/layout.rb
+++ lib/localized_templates/actioncontroller/layout.rb
@@ -1,8 +1,12 @@
-ActionController::Base.class_eval do
-  private
-    def layout_directory?(layout_name)
-      @template.__send__(:_pick_template, "#{File.join('layouts', layout_name)}.#{@template.template_format}", I18n.locale) ? true : false
-    rescue ActionView::MissingTemplate
-      false
-    end
+module ActionController
+  class Base
+
+    private
+      def layout_directory?(layout_name)
+        @template.__send__(:_pick_template, "#{File.join('layouts', layout_name)}.#{@template.template_format}", I18n.locale) ? true : false
+      rescue ActionView::MissingTemplate
+        false
+      end
+
+  end
 end
